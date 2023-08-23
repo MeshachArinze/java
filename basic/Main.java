31b0c1b3ab54ea55f9c6a19248c0fc941eb25652
MeshachArinze <meshacharinze@gmail.com>
2023-08-23 15:07:20 +0100

    commit to repo

--- basic/Main.java
+++ basic/Main.java
@@ -1,20 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        int isAdult = 40;
-        int isYouth = 24;
-        
+    
 
-        String Adult = "Candidate is an adult";
-        String Youth = "Candidate is a Youth";
-        String Child = "Candidate is a Child";
+        String[] cars  = {"Volve", "BMW", "BENZ"};
 
-        if (!(isAdult >= 40 && isYouth < 40)) {
-            System.out.println(Adult);
-        } else if (isYouth <= 24) {
-            System.out.println(Youth);
-        } else {
-            System.out.println(Child);
+        for (String i : cars) {
+            System.out.println(i);
         }
     }
 }
