95a20a9bf8c778935625658270adf70fe3b24ad9
MeshachArinze <meshacharinze@gmail.com>
2023-08-23 15:10:52 +0100

    commit to repo

--- basic/Main.java
+++ basic/Main.java
@@ -1,12 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
-    
 
-        String[] cars  = {"Volve", "BMW", "BENZ"};
+        // String[] cars = {"Volve", "BMW", "BENZ"};
 
-        for (String i : cars) {
+        // for (String i : cars) {
+        // System.out.println(i);
+        // }
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 4) {
+                break;
+            }
             System.out.println(i);
+
         }
     }
 }
