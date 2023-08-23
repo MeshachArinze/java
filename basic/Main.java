8bce6984758cadffdd51ba5450839463980c1565
MeshachArinze <meshacharinze@gmail.com>
2023-08-24 00:40:02 +0100

    commit to main

--- basic/Main.java
+++ basic/Main.java
@@ -1,16 +1,9 @@
 public class Main {
-  static int plusMethodInt(int x, int y) {
-    return x + y;
-  }
-  
-  static double plusMethodDouble(double x, double y) {
-    return x + y;
-  }
-  
+  int x = 10;
+
   public static void main(String[] args) {
-    int myNum1 = plusMethodInt(8, 5);
-    double myNum2 = plusMethodDouble(4.3, 6.26);
-    System.out.println("int: " + myNum1);
-    System.out.println("double: " + myNum2);
+    Main myObj = new Main();
+    myObj.x = 25; // x is now 25
+    System.out.println(myObj.x);
   }
 }
