ccf62632ee5cbc4991aae91dba6ec1c9b30122fe
MeshachArinze <meshacharinze@gmail.com>
2023-08-24 01:50:41 +0100

    commit to main

--- basic/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  int x = 10;
-
-  public static void main(String[] args) {
-    Main myObj = new Main();
-    myObj.x = 25; // x is now 25
-    System.out.println(myObj.x);
-  }
-}
